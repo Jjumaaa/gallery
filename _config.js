f7ba08a6b73c15ccad19386729d2c6f018485428
Juma <gerald.juma@student.moringaschool.com>
2025-09-27 09:29:14 +0300

    Updated config to point to env

--- _config.js
+++ _config.js
@@ -1,10 +1,18 @@
+require('dotenv').config();
 var config = {}
+
+const {username = process.env.MONGOUSER,
+    userpassword = process.env.MONGOPASSWORD,
+    mongocluster = process.env.MONGOHOST,
+    prod_env = process.env.MONGOPRODUCTIONDATABASE,
+    dev_env = process.env.MONGODEVDATABASE,
+    test_env = process.env.MONGOTESTDATABASE} = process.env;
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://Jjumaaa:jumajuma06@cluster0.vqjy6ia.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://Jjumaaa:jumajuma06@cluster0.vqjy6ia.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://Jjumaaa:jumajuma06@cluster0.vqjy6ia.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: `mongodb+srv://${username}:${userpassword}@${mongocluster}/${prod_env}?retryWrites=true&w=majority`,
+    development: `mongodb+srv://${username}:${userpassword}@${mongocluster}/${dev_env}?retryWrites=true&w=majority`,
+    test: `mongodb+srv://${username}:${userpassword}@${mongocluster}/${test_env}?retryWrites=true&w=majority`,
 
 }
 module.exports = config;
